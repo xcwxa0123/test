7a956d21a6cd4c0d445db2d8345ae00cfc29e0d2
Alamorn <xcwxa0123@hotmail.com>
2019-10-25 10:39:12 +0800

    测试smartGit

--- vueTest/dropLine.js
+++ vueTest/dropLine.js
@@ -1,13 +1,13 @@
 
       !function () {
         //获取dom对象对应属性名property的值，若没有该property则返回传入的t
-        function n(obj, property, defaultValue) {
+        function getSetProperty(obj, property, defaultValue) {
   
           return obj.getAttribute(property) || defaultValue
   
         }
         //根据标签的名字tagName返回该DOM对象
-        function e(tagName) {
+        function getElementsByTagName(tagName) {
   
           return document.getElementsByTagName(tagName)
   
@@ -22,16 +22,13 @@
           * n count值
         */
         // 的对象
-        function t() {
+        function getScriptObj() {
   
-          var t = e("script"), o = t.length, i = t[o - 1];
-          console.log(e("script"));
-          console.log(e("script").length);
-          console.log(e("script")[e("script").length - 1]);
+          var scriptArr = getElementsByTagName("script"), scriptLength = scriptArr.length, scriptObj = scriptArr[scriptLength - 1];
   
           return {
   
-            l: o, z: n(i, "zIndex", 9999999), o: n(i, "opacity", .8), c: n(i, "color", "0,0,0"), n: n(i, "count", 99)
+            l: scriptLength, z: getSetProperty(scriptObj, "zIndex", 9999999), o: getSetProperty(scriptObj, "opacity", .8), c: getSetProperty(scriptObj, "color", "0,0,0"), n: getSetProperty(scriptObj, "count", 99)
   
           }
   
@@ -55,10 +52,12 @@
           s.forEach(function (i, x) {
   
             for (i.x += i.xa, i.y += i.ya, i.xa *= i.x > a || i.x < 0 ? -1 : 1, i.ya *= i.y > c || i.y < 0 ? -1 : 1, r.fillRect(i.x - .5, i.y - .5, 1, 1), e = x + 1; e < u.length; e++)n = u[e],
-  
+              //i.x为当前元素的x坐标，n.x为下一元素的x坐标，则o为当前元素和下一元素的x坐标差，m为y坐标差
               null !== n.x && null !== n.y && (o = i.x - n.x, m = i.y - n.y,
-  
-                l = o * o + m * m, l < n.max && (n === y && l >= n.max / 2 && (i.x -= .03 * o, i.y -= .03 * m),
+                //l为坐标差的平方和
+                l = o * o + m * m, 
+                
+                l < n.max && (n === y && l >= n.max / 2 && (i.x -= .03 * o, i.y -= .03 * m),
   
                   t = (n.max - l) / n.max, r.beginPath(), r.lineWidth = t / 2, r.strokeStyle = "rgba(" + d.c + "," + (t + .2) + ")", r.moveTo(i.x, i.y), r.lineTo(n.x, n.y), r.stroke()))
   
@@ -72,7 +71,7 @@
           //d 本界面动画参数：scrpit对象长度 count color 透明度 zindex
           //l = c_n1
           //r = 用canvas m 创建画笔r
-          d = t(), l = "c_n" + d.l, r = m.getContext("2d"),
+          d = getScriptObj(), l = "c_n" + d.l, r = m.getContext("2d"),
           //指定x为动画绘制函数
           x = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
   
@@ -83,12 +82,12 @@
             },
           //指定w为随机数，
           w = Math.random, 
-          y = { x: null, y: null, max: 2e4 }; 
+          y = { x: null, y: null, max: 6e4 }; 
           //m.id = c_n1
           m.id = l, 
           m.style.cssText = "position:fixed;top:0;left:0;z-index:" + d.z + ";opacity:" + d.o, 
           //动画画布画在body下
-          e("html")[0].appendChild(m), 
+          getElementsByTagName("html")[0].appendChild(m), 
           //a为m.width,c为m.height
           o(), 
           window.οnresize = o,
@@ -116,4 +115,4 @@
           setTimeout(function () { i() }, 100)
   
       }();
-  
+      
